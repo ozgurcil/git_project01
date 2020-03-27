0aff4d826f092649b7cd4aafe8f6ac3d05111787
ozgur cil <ozgurcil90@gmail.com>
2020-03-27 18:58:32 -0400

    ninth commit

--- src/gitPackage/NextClass.java
+++ src/gitPackage/NextClass.java
@@ -5,6 +5,9 @@ public class NextClass {
 	public static void main(String[] args) {
 		System.out.println("just a test 2 use terminal with git");
 		System.out.println("commit eight");
+		System.out.println("tired");
+		System.out.println("testing this terminal and git is ehhhh");
+		
 	}
 
 }

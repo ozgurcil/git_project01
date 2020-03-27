a1523c5e1d1eb8c1b767ef9a9a5c9eb3712d5428
ozgur cil <ozgurcil90@gmail.com>
2020-03-27 18:46:53 -0400

    sixth commit

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -6,6 +6,7 @@ public class GitDay2 {
 	System.out.println("hello world");
 	System.out.println("hello ozgur");
 	System.out.println("hello git users");
+	System.out.println("check if your local is  ahead of origin master");
 	}
 
 }

6e0716ef8b494202527eca496229c57976de2267
ozgur cil <ozgurcil90@gmail.com>
2020-03-27 18:37:39 -0400

    our fifth commit

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -5,6 +5,7 @@ public class GitDay2 {
 	public static void main(String[] args) {
 	System.out.println("hello world");
 	System.out.println("hello ozgur");
+	System.out.println("hello git users");
 	}
 
 }
